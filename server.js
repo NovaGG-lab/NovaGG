5e2a10731582e92b72cd1f42ec3841b46a0eef25
NovaGG-lab <skeletonbeing23@gmail.com>
2025-05-30 14:53:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 
-http.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
-});
-
-const { Server } = require('socket.io');
-
-const server = http.createServer(app);
-
 const players = {};
 const bannedWords = ['fuck', 'shit', 'bitch', 'ass'];
 
@@ -27,24 +19,22 @@ io.on("connection", (socket) => {
   console.log("User connected", socket.id);
 
   socket.on("join", ({ username }) => {
-  players[socket.id] = {
-    id: socket.id,
-    username,
-    position: { x: 0, z: 0 },
-  };
-  io.emit("playerData", Object.values(players));
-  io.emit("system", `${username} joined the game!`);
-});
-
+    players[socket.id] = {
+      id: socket.id,
+      username,
+      position: { x: 0, z: 0 },
+    };
+    io.emit("playerData", Object.values(players));
+    io.emit("system", `${username} joined the game!`);
+  });
 
   socket.on("move", ({ x, z, rotationY }) => {
-  if (players[socket.id]) {
-    players[socket.id].position = { x, z };
-    players[socket.id].rotationY = rotationY;
-    socket.broadcast.emit("playerMoved", { id: socket.id, position: { x, z }, rotationY });
-  }
-});
-
+    if (players[socket.id]) {
+      players[socket.id].position = { x, z };
+      players[socket.id].rotationY = rotationY;
+      socket.broadcast.emit("playerMoved", { id: socket.id, position: { x, z }, rotationY });
+    }
+  });
 
   socket.on("chat", (msg) => {
     const player = players[socket.id];
@@ -63,6 +53,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", disconnectPlayer);
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// ✅ This is the ONLY server listener now
+http.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
